563c77378e2b2bc1e776830ccac0d26c9133fb0a
Suraj Kumar <suraj.k@avantaifintech.com>
2025-09-09 22:47:23 +0530

    feat: user register and login api is created

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,54 @@
+import jwt from "jsonwebtoken";
+
 import Users from "../models/user.js";
 import AppError from "../utils/appError.js";
 import { catchAsync } from "../utils/catchAsync.js";
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user.id);
+  const options = {
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+  };
+
+  if (process.env.NODE_ENV === "production") options.secure = true;
+
+  res.cookie("token", token, options);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+export const signup = catchAsync(async (req, res, next) => {
+  const data = req.body;
+  const newUser = await Users.create(data);
+
+  createSendToken(newUser, 201, res);
+});
+
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  const user = await Users.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+  createSendToken(user, 200, res);
+});
