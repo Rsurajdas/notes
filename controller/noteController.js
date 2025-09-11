40c377754909e609160d6e9b334d1a75e1bd26e0
Suraj Kumar <suraj.k@avantaifintech.com>
2025-09-11 06:09:51 +0530

    feat: update archive status & delete note api created and updated get all notes api to get archived notes by query

--- controller/noteController.js
+++ controller/noteController.js
@@ -23,7 +23,14 @@ export const createNote = catchAsync(async (req, res, next) => {
 
 export const getAllNotes = catchAsync(async (req, res, next) => {
   const userId = req.user.id;
-  const notes = await Notes.find({ user: userId, isTrashed: false }).sort({
+  const { archived } = req.query;
+  const isArchived = !!archived;
+
+  const notes = await Notes.find({
+    user: userId,
+    isTrashed: false,
+    isArchived,
+  }).sort({
     createdAt: -1,
   });
   res.status(200).json({
@@ -50,5 +57,51 @@ export const getNote = catchAsync(async (req, res, next) => {
     data: {
       note,
     },
+  });
+});
+
+export const updateArchiveStatus = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+  const { isArchived } = req.body;
+  const note = await Notes.findByIdAndUpdate(
+    {
+      _id: id,
+      user: userId,
+      isTrashed: false,
+    },
+    { isArchived },
+    { new: true, runValidators: true }
+  );
+
+  if (!note) {
+    return next(new AppError("No note found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      note,
+    },
+  });
+});
+
+export const deleteNote = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const id = req.params.id;
+
+  const note = await Notes.findByIdAndUpdate(
+    { _id: id, user: userId, isTrashed: false },
+    { isTrashed: true },
+    { new: true, runValidators: true }
+  );
+
+  if (!note) {
+    return next(new AppError("No note found with that ID", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
